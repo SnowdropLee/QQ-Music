270c2f98b7de1d2ffe22106e0719b64d53be61b9
snowdrop <522347325@qq.com>
2018-09-04 22:53:59 +0800

    Update contansts.js

--- js/contansts.js
+++ js/contansts.js
@@ -1,4 +1,4 @@
-export const LYRIC_URL = 'https://qq-music-api.now.sh/lyrics'
+export const LYRIC_URL = '`https://qq-server-kfhdengskt.now.sh/lyrics'
 export const SEARCH_URL = 'https://qq-music-api.now.sh/search'
 export const RANK_URL = 'http://localhost:4000/rank'
 export const RECOMEND_URL = 'https://qq-music-api.now.sh/'
